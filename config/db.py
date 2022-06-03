18aea697dc60b20d2342a7619096b83fe2e7fd7c
Eustacio <arellanos.baaeus@gmail.com>
2022-06-02 22:01:00 -0500

    Cambio de parametros en archivo db.py para conexion de la base

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:@localhost:3306/dbmails")
+#engine = create_engine("mysql+pymysql://root:@localhost:3306/dbmails")
+engine = create_engine("mysql+pymysql://admin_aws:db_mails05!_@dbmails.cqguuowfmb5g.us-west-1.rds.amazonaws.com:3306/db_mails")
 
 meta = MetaData()
 
